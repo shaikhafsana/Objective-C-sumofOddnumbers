69738b42398df57de5d6b738b7a738a77b02e019
Afsana Shaikh <afsanashaikh2491@gmail.com>
2016-10-21 16:45:56 +0530

    Modified code

--- ASOddNumberSum/main.m
+++ ASOddNumberSum/main.m
@@ -8,20 +8,32 @@
 
 #import <Foundation/Foundation.h>
 void sumodd(int initial,int limit);
+void oddnumber(int initial,int limit);
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
         int initial=1,limit=20;
+        oddnumber(initial, limit);
         sumodd(initial,limit);
         
     }
     return 0;
 }
-// function
+//To print 1st 10 odd numbers
+void oddnumber(int initial,int limit)
+{
+     printf("1st Ten odd numbers Are:\n");
+    for (int i=initial;i<=limit;i+=2) {
+       
+        printf("%d\n",i);
+    }
+}
+// function to sum
 void sumodd(int initial,int limit)
 {
     int sum=0;
     for (int i=initial;i<=limit;i+=2) {
+    
         sum += i;
     }
     printf("Sum of first 10 Odd no is%d:\n",sum);
